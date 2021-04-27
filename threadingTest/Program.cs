607783a75b1258d7b9cc3468227d46b918e45297
Sixtten610 <60339623+Sixtten610@users.noreply.github.com>
2021-04-27 10:38:22 +0200

    yes

---
+++ threadingTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace threadingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
